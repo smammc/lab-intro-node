291b40d7c4e9fb00b0e009c73643fca55ac830db
smammc <smammc@hotmail.com>
2024-07-29 17:02:30 +0100

    Fineshed lab

--- index.js
+++ index.js
@@ -1,17 +1,59 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
 
-  add(item) {}
+  add(item) {
+    if (this.items.length === 0) {
+      this.items.splice(0, 0, item);
+    } else {
+      this.items.forEach((arrayItem, index) => {
+        if (item < arrayItem) {
+          this.items.splice(index - 1, 0, item);
+        }
+      });
+      this.length = this.items.length;
+    }
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos > this.items.length) {
+      throw new Error("OutOfBounds");
+    }
+    return this.items[pos];
+  }
 
-  max() {}
+  max() {
+    if (!this.items) {
+      throw new Error("EmptySortedList");
+    }
+    return this.items[this.items.length - 1];
+  }
 
-  min() {}
+  min() {
+    if (!this.items) {
+      throw new Error("EmptySortedList");
+    }
+    return this.items[0];
+  }
 
-  sum() {}
+  sum() {
+    return this.items.reduce((acc, val) => {
+      acc += val;
+    }, 0);
+  }
 
-  avg() {}
+  avg() {
+    if (!this.items) {
+      throw new Error("EmptySortedList");
+    }
+    return (
+      this.items.reduce((acc, val) => {
+        acc += val;
+      }, 0) / this.items.length
+    );
+  }
 }
 
 module.exports = SortedList;
